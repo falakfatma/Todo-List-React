ce88b9e16526bcaeb553ecb89eebb34dd33e6462
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-10-07 13:11:55 +0000

    Remove Some Element

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import List from "./List"
 export default function todo() {
   const [itemName, setItemName] = React.useState("");
-  const [itemKey, setItemKey] = React.useState(0);
-  const [deleteItem, setdeleteItem] = useState();
   const [Item, setItem] = useState([]);
   function searchHandlerFunction(e) {
     setItemName(e.target.value)
